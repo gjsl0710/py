a3ab6404c9bc14d8e506e2b96e1bd30a6bc4db22
gjsl0710 <67987471+gjsl0710@users.noreply.github.com>
2020-12-15 17:24:34 +0900

    add

--- bot.py
+++ bot.py
@@ -1,84 +1,21 @@
 import discord
-import asyncio
-import os
 from discord.ext import commands
-import urllib
-from urllib.request import URLError
-from urllib.request import HTTPError
-from urllib.request import urlopen
-from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup
-from urllib.parse import quote
-import re # Regex for youtube link
-import warnings
-import requests
-import time
+import os
 
-client = discord.Client() # Create Instance of Client. This Client is discord server's connection to Discord Room
-bottoken = ""
-
-
-@client.event # Use these decorator to register an event.
-async def on_ready(): # on_ready() event : when the bot has finised logging in and setting things up
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("!코로나"))
-    print("New log in as {0.user}".format(client))
+client = commands.Bot(command_prefix = '-')
 
 @client.event
-async def on_message(message): # on_message() event : when the bot has recieved a message
-    #To user who sent message
-    # await message.author.send(msg)
-    print(message.content)
-    if message.author == client.user:
-        return
+async def on_ready():
 
-    if message.content.startswith("!코로나"):
-        # 보건복지부 코로나 바이러스 정보사이트"
-        covidSite = "http://ncov.mohw.go.kr/index.jsp"
-        covidNotice = "http://ncov.mohw.go.kr"
-        html = urlopen(covidSite)
-        bs = BeautifulSoup(html, 'html.parser')
-        latestupdateTime = bs.find('span', {'class': "livedate"}).text.split(',')[0][1:].split('.')
-        statisticalNumbers = bs.findAll('span', {'class': 'num'})
-        beforedayNumbers = bs.findAll('span', {'class': 'before'})
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.online)
 
-        #주요 브리핑 및 뉴스링크
-        briefTasks = []
-        mainbrief = bs.findAll('a',{'href' : re.compile('\/tcmBoardView\.do\?contSeq=[0-9]*')})
-        for brf in mainbrief:
-            container = []
-            container.append(brf.text)
-            container.append(covidNotice + brf['href'])
-            briefTasks.append(container)
-        print(briefTasks)
-
-        # 통계수치
-        statNum = []
-        # 전일대비 수치
-        beforeNum = []
-        for num in range(7):
-            statNum.append(statisticalNumbers[num].text)
-        for num in range(4):
-            beforeNum.append(beforedayNumbers[num].text.split('(')[-1].split(')')[0])
-
-        totalPeopletoInt = statNum[0].split(')')[-1].split(',')
-        tpInt = ''.join(totalPeopletoInt)
-        lethatRate = round((int(statNum[3]) / int(tpInt)) * 100, 2)
-        embed = discord.Embed(title="코로나19 현황", description="",color=0x5CD1E5,url="http://ncov.mohw.go.kr/index.jsp")
-        embed.add_field(name="현황기준",value="해당 자료는 " + latestupdateTime[0] + "월 " + latestupdateTime[1] + "일 "+latestupdateTime[2] +" 자료입니다.", inline=False)
-        embed.set_author(name="아래 제목을 클릭해 더 많은정보를 확인할 수 있습니다.",icon_url=message.author.avatar_url)
-        embed.add_field(name="확진환자(누적)", value=statNum[0].split(')')[-1]+"("+beforeNum[0]+")",inline=True)
-        embed.add_field(name="완치환자(격리해제)", value=statNum[1] + "(" + beforeNum[1] + ")", inline=True)
-        embed.add_field(name="치료중(격리 중)", value=statNum[2] + "(" + beforeNum[2] + ")", inline=True)
-        embed.add_field(name="사망", value=statNum[3] + "(" + beforeNum[3] + ")", inline=True)
-        embed.add_field(name="누적확진률", value=statNum[6], inline=True)
-        embed.add_field(name="치사율", value=str(lethatRate) + " %",inline=True)
-        embed.add_field(name="관련 브리핑" + briefTasks[0][0],value="Link : " + briefTasks[0][1],inline=False)
-        embed.add_field(name="관련 브리핑" + briefTasks[1][0], value="Link : " + briefTasks[1][1], inline=False)
-        embed.set_thumbnail(url="https://wikis.krsocsci.org/images/7/79/%EB%8C%80%ED%95%9C%EC%99%95%EA%B5%AD_%ED%83%9C%EA%B7%B9%EA%B8%B0.jpg")
-        embed.set_footer(text='출처 : 보건복지부',
-                         icon_url='https://cdn.discordapp.com/attachments/757231148933054467/788307985255628831/Y5Tg6aur.jpg')
-        await message.channel.send("대한민국 코로나-19 현황", embed=embed)
-        
+  await client.change_presence(activity=discord.Game(name="게임 하는중"))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+  
+  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 
 client.run(os.environ['token'])
